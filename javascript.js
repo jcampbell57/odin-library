960773c6fda81c452fbaa4434f96d0d802a363d4
jcampbell <jcampbell57@gmail.com>
2022-06-29 16:50:57 -0700

    adds placeholder elements

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
 
+let myLibrary = [];
+
 // object constructor for new book
 function book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}.`
-    }; 
 }
+
+book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
+}; 
+
+function addBookToLibrary() {
+    let newBook = object.create(book.prototype)
+}
+
+for (book in myLibrary) {
+}
+
+
+const book1 = new book('How to WIN', 'Jason Campbell', '666', 'hella read that');
+
+book1.info();
