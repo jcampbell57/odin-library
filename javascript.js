2a566bd2a34f14fd3892bf4e0f98b26d8dbe9c4b
jcampbell <jcampbell57@gmail.com>
2022-06-30 15:11:19 -0700

    submit book now populates card with book info

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 // page elements
 const addBookBtn = document.querySelector('.addBookBtn');
 const submitBookBtn = document.getElementById('submitBookBtn');
-const bookStatusBtn = document.querySelector('.bookStatusBtn');
+const bookStatusBtn = document.querySelectorAll('.bookStatusBtn');
+
+const cardContainer = document.querySelector('.cardContainer')
 const card = document.querySelector('.card');
-const removeCard = document.getElementById('removeCard');
+const removeCard = document.querySelectorAll('#removeCard');
+
 const popup = document.querySelector('.popup');
 const closePopup = document.getElementById('closePopup');
 const bookTitle = document.getElementById('bookTitle');
@@ -51,34 +54,85 @@ closePopup.addEventListener('click', () => {
     popup.id = 'noNewBook';
 })
 
+
+
 // submit book
 submitBookBtn.addEventListener('click', (e) => {
+    
     e.preventDefault();
-    console.log(e);
-    console.log(bookTitle);
-    console.log(bookAuthor.textContent);
-    console.log(bookPages.textContent);
-    console.log(bookStatus.textContent);
+    console.log(bookTitle.value);
+    console.log(bookAuthor.value);
+    console.log(bookPages.value);
+    console.log(bookStatus.value);
     popup.id = 'noNewBook';
 
+    // create new card
+    const newCard = document.createElement('div');
+    newCard.setAttribute('class', 'card');
+    newCard.setAttribute('id', 'notRead');
+    // cardContainer.appendChild(newCard);
+
+    //populate card with book info
+    const newCloseContainer = document.createElement('div');
+    newCloseContainer.setAttribute('class', 'closeContainer');
+    newCloseContainer.innerHTML = "<img src='assets/delete.svg' alt='delete' id='removeCard'>";
+
+    const newBookTitle = document.createElement('span');
+    newBookTitle.setAttribute('class', 'bookTitle');
+    newBookTitle.textContent = 'by ' + bookTitle.value;
+
+    const newBookAuthor = document.createElement('span');
+    newBookAuthor.setAttribute('class', 'bookAuthor');
+    newBookAuthor.textContent = bookAuthor.value;
+
+    const newBookPages = document.createElement('span');
+    newBookPages.setAttribute('class', 'bookPages');
+    newBookPages.textContent = bookPages.value = 'pages';
+
+    const newBookStatus = document.createElement('button');
+    newBookStatus.setAttribute('class', 'bookStatusBtn');
+    newBookStatus.setAttribute('id', 'notRead');
+    newBookStatus.textContent = 'Not read!';
+
+
+    // append book info to card
+    newCard.appendChild(newCloseContainer);
+    newCard.appendChild(newBookTitle);
+    newCard.appendChild(newBookAuthor);
+    newCard.appendChild(newBookPages);
+    newCard.appendChild(newBookStatus);    
+
+    // append card to page
+    cardContainer.appendChild(newCard);
 })
+
+
 
 // toggle book status
-bookStatusBtn.addEventListener('click', (e) => {
-    if (e.target.id === 'read') {
-        card.id = 'notRead'
-        bookStatusBtn.id = 'notRead'
-        bookStatusBtn.textContent = 'Not read!'
-    } else if (e.target.id === 'notRead') {
-        card.id = 'read'
-        bookStatusBtn.id = 'read'
-        bookStatusBtn.textContent = 'Read!'
-    } else {
-        console.log('this is weird')
-    }
+bookStatusBtn.forEach(Btn => {
+    Btn.addEventListener('click', (e) => {
+        if (e.target.id === 'read') {
+            card.id = 'notRead'
+            bookStatusBtn.id = 'notRead'
+            bookStatusBtn.textContent = 'Not read!'
+        } else if (e.target.id === 'notRead') {
+            card.id = 'read'
+            bookStatusBtn.id = 'read'
+            bookStatusBtn.textContent = 'Read!'
+        } else {
+            console.log('this is weird')
+        }
+    })
 })
 
+
+
 // remove book / card
-removeCard.addEventListener('click', (e) => {
-    e.path[2].remove();
-})
+removeCard.forEach(Btn => {
+    Btn.addEventListener('click', (e) => {
+        //need to figure out new way
+        // console.log(e.composedPath())
+        // old way
+        e.path[2].remove();
+    })
+}) 
