7977ac2b6fa3e27372cc8ba89067e8179544b859
jcampbell57 <jcampbell57@gmail.com>
2024-03-13 12:41:21 -0700

    add dark mode toggle, functional stats display

--- v2/javascript.js
+++ v2/javascript.js
@@ -1,4 +1,30 @@
-// page elements
+// Create page elements
+
+const createFooter = () => {
+  const footer = document.querySelector('footer');
+  footer.classList.add('light')
+
+  const copyright = document.createElement('p');
+  copyright.textContent = `Copyright © ${new Date().getFullYear()} jcampbell57`;
+
+  const githubLink = document.createElement('a');
+  githubLink.href = 'https://github.com/jcampbell57';
+  githubLink.target = '_blank';
+
+  const newGithubIcon = document.createElement('img');
+  newGithubIcon.src = '../assets/GitHub-32px.png';
+  newGithubIcon.classList.add('github', 'SVGlight');
+
+  githubLink.appendChild(newGithubIcon);
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
+};
+
+createFooter();
+
+
+// Grab page elements
+
 const bookList = document.getElementById('bookList')
 
 
@@ -35,6 +61,7 @@ function displayBooks() {
   myLibrary.forEach((book, index) => {
     // Create new row
     const newRow = document.createElement('tr')
+    newRow.classList.add(book.isRead ? 'read' : 'notRead')
 
     // Add title cell
     const titleCell = document.createElement('td');
@@ -76,6 +103,15 @@ function displayBooks() {
       newButton.classList.toggle('read')
       newButton.classList.toggle('notRead')
       newButton.textContent = newButton.classList.contains('read') ? 'Read!' : 'Not read!';
+      bookCount = document.getElementById('bookCount');
+      pageCount = document.getElementById('pageCount');
+      if (newButton.classList.contains('read')) {
+        bookCount.textContent = parseInt(bookCount.textContent) + 1;
+        pageCount.textContent = parseInt(pageCount.textContent) + (book.pages);
+      } else {
+        bookCount.textContent = parseInt(bookCount.textContent) - 1;
+        pageCount.textContent = parseInt(pageCount.textContent) - (book.pages);
+      }
     };
     
     newButton.addEventListener('click', toggleStatus);
@@ -87,12 +123,18 @@ function displayBooks() {
     removeBook.classList.add('removeBook');
     
     const newImg = document.createElement('img');
+    newImg.classList.add('removeBookIcon')
     newImg.src = '../assets/delete.svg';
     newImg.alt = 'delete';
     
     const deleteBook = (row) => {
-      // const titleToRemove = row.querySelector('.bookTitle').textContent;
-      // const indexToRemove = myLibrary.findIndex(item => item.title === titleToRemove);
+      if (row.classList.contains('read')) {
+        bookCount = document.getElementById('bookCount');
+        pageCount = document.getElementById('pageCount');
+        bookCount.textContent = parseInt(bookCount.textContent) - 1;
+        pageCount.textContent = parseInt(pageCount.textContent) - parseInt(row.querySelector('.bookPages').textContent);
+      }
+
       if (index !== -1) {
           myLibrary.splice(index, 1);
       }
@@ -115,29 +157,8 @@ function displayBooks() {
 }
 
 
-const createFooter = () => {
-  const footer = document.querySelector('footer');
+// Add placeholder content
 
-  const copyright = document.createElement('p');
-  copyright.textContent = `Copyright © ${new Date().getFullYear()} jcampbell57`;
-
-  const githubLink = document.createElement('a');
-  githubLink.href = 'https://github.com/jcampbell57';
-  githubLink.target = '_blank';
-
-  const newGithubIcon = document.createElement('img');
-  newGithubIcon.src = '../assets/GitHub-32px.png';
-  newGithubIcon.setAttribute('class', 'github');
-
-  githubLink.appendChild(newGithubIcon);
-  footer.appendChild(copyright);
-  footer.appendChild(githubLink);
-};
-
-createFooter();
-
-
-// Placeholder content
 book1 = new Book('The Intelligent Investor', 'Benjamin Graham', 1949, 460);
 addBookToLibrary(book1);
 // console.log(book1.info());
@@ -150,5 +171,62 @@ book3 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 295);
 addBookToLibrary(book3);
 // console.log(book3.info());
 
-
 displayBooks();
+
+
+// Grab page elements
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const logo = document.getElementById('logo');
+const themeToggle = document.getElementById('themeToggle')
+const table = document.querySelector('table')
+const thead = document.querySelector('thead')
+const addBookBtn = document.querySelector('.addBookBtn')
+const removeBookIcons = document.querySelectorAll('.removeBookIcon')
+const stats = document.getElementById('stats');
+const footer = document.querySelector('footer');
+const githubIcon = document.querySelector('.github');
+
+
+// Add event listeners
+
+themeToggle.addEventListener('mouseenter', (e) => {
+  themeToggle.classList.toggle('SVGlight')
+});
+
+themeToggle.addEventListener('mouseout', (e) => {
+  themeToggle.classList.toggle('SVGlight')
+});
+
+// Dark mode toggler
+const projectsHeader = document.querySelector('.projectsHeader');
+const themeToggleIcon = document.querySelector('.themeToggleIcon');
+const themeToggler = document.querySelector('.themeToggle');
+
+function toggleDarkMode() {
+  body.classList.toggle('light')
+  header.classList.toggle('light');
+  logo.classList.toggle('SVGlight')
+  themeToggle.classList.toggle('SVGlight')
+  table.classList.toggle('light')
+  thead.classList.toggle('light')
+  addBookBtn.classList.toggle('light')
+  stats.classList.toggle('light');  
+  footer.classList.toggle('light');  
+  githubIcon.classList.toggle('SVGlight');
+  removeBookIcons.forEach(icon => {
+      icon.classList.toggle('SVGlight');
+  });
+}
+
+themeToggle.addEventListener('mousedown', function () {
+  toggleDarkMode();
+});
+
+themeToggle.addEventListener('keydown', function (event) {
+  // Check if the key pressed is 'Enter' (key code 13) or 'Space' (key code 32)
+  if (event.key === 'Enter' || event.key === ' ') {
+    event.preventDefault();
+    toggleDarkMode();
+  }
+});
